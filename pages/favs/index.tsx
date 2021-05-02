d29033389b17dca975264eb3046526d4f8045ffc
miily8310s <rillakerika@gmail.com>
2021-05-03 08:58:28 +0900

    add favs page

---
+++ pages/favs/index.tsx
@@ -0,0 +1,9 @@
+const Favs = (): JSX.Element => {
+  return (
+    <div>
+      <h1>All My Favs</h1>
+    </div>
+  )
+}
+
+export default Favs
---
+++ pages/favs/test.tsx
@@ -0,0 +1,9 @@
+const Test = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Test page</h1>
+    </div>
+  )
+}
+
+export default Test

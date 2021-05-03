f92b92e848bb137c422c897f5f4dd2a44d059798
miily8310s <rillakerika@gmail.com>
2021-05-04 07:49:49 +0900

    add google books api fetch

--- pages/favs/index.tsx
+++ pages/favs/index.tsx
@@ -1,7 +1,34 @@
-const Favs = (): JSX.Element => {
+import { GetStaticProps } from 'next'
+import styles from '../../styles/Favs.module.css'
+
+interface Book {
+  id: number
+  volumeInfo: {
+    title: string
+  }
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(
+    'https://www.googleapis.com/books/v1/volumes?q=TypeScript'
+  )
+  const data = await res.json()
+  return {
+    props: { books: data.items },
+  }
+}
+
+const Favs = ({ books }: { books: Book[] }): JSX.Element => {
   return (
     <div>
       <h1>All My Favs</h1>
+      {books.map((book) => (
+        <div key={book.id}>
+          <a className={styles.book}>
+            <h3>{book.volumeInfo.title}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   )
 }
